bbf2bb3510f6a45b951c365966669c927e7a8aeb
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2018-11-24 16:57:48 +0200

    README

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,6 +1,6 @@
 const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 export const Config = {
-    API_URL: env === 'development' ? 'http://localhost:5000/api' : 'https://movieholic-api.herokuapp.com/api',
+    API_URL: env === 'development' ? 'https://movieholic-api.herokuapp.com/api' : 'https://movieholic-api.herokuapp.com/api',
     STRIPE_KEY: env === 'development' ? 'pk_test_kV0XNSe1xcNhGWFYtuRXHnLt' : process.env.STRIPE_KEY
 };
